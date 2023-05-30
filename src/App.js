7c5dc75562066070f38a91c05027ffea06d688bf
Rish_Tomar <rishabh.tomar2@gmail.com>
2023-05-30 15:11:51 +0530

    Addded create list button

--- src/App.js
+++ src/App.js
@@ -12,6 +12,36 @@ const Container=styled.div`
 
 function App() {
   const [data,setData]=useState(initialData)
+  const [listTitle,setListTitle]=useState()
+
+  const handleChange=(e)=>{
+    console.log(e.target.value)
+    setListTitle(e.target.value)
+  }
+
+  const createList=()=>{
+    const columns=data.columns;
+    const columnOrder=data.columnOrder
+    const newColumnID=columnOrder.length+1;
+
+    const newColumn=`column-${newColumnID}`
+    // update
+    columnOrder.push(newColumn);
+    columns.newColumn={
+      id:newColumn,
+      title:listTitle,
+      taskIds:[],
+    }
+
+    const newState={
+      ...data,
+      columns,
+      columnOrder
+    }
+    setData(newState)
+    console.log(newState,data)
+
+  }
 
   const onDragEnd =result=>{
     console.log(result)
@@ -75,12 +105,17 @@ function App() {
       {/* <div className="App"> */}
         { data.columnOrder.map((columnId)=>{
               const column = data.columns[columnId]
+              {console.log(columnId)}
               const tasks   =column.taskIds.map(taskId=> data.tasks[taskId])
-
               return <Column key={column.id} column={column} tasks={tasks}/> 
           })
         }
       {/* </div> */}
+      <div>
+        <p>Create New List</p>
+        <input type="text" onChange={handleChange}/>
+        <button onClick={createList}>Click</button>
+      </div>
     </Container>
    </DragDropContext>
   );
